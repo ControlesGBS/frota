818b0c94ff5a69277818b43e87ed24602b576971
Eliezer <controle@gbsservicos.com>
2026-06-03 22:45:06 -0300

    Corrige referencia imagem Moto maiusculo

--- src/components/shared/Topbar.jsx
+++ src/components/shared/Topbar.jsx
@@ -13,7 +13,7 @@ export default function Topbar({ name, sub, darkMode, onToggleDark }) {
           background: '#f0f0ec',
           display: 'flex', alignItems: 'center', justifyContent: 'center'
         }}>
-          <img src="/moto.jpg" alt="Moto" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+          <img src="/Moto.jpg" alt="Moto" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
         </div>
         <div>
           <div className="topbar-name">{name}</div>
